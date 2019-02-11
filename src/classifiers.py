f77f263cb142fb350dbc03e7e1ca05584cc966a2
KyleY <kyle.yu.96@outlook.com>
2019-02-11 18:49:00 -0500

    .

--- src/classifiers.py
+++ src/classifiers.py
@@ -16,12 +16,12 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=trainSize, 
 lg = LogisticRegression(penalty="l2", random_state=randomState)
 lg.fit(x_train, y_train)
 y_pred_lg = lg.predict(x_test)
-print("Mean accuracy is ", lg.score(x_test, y_test))
+print("Mean accuracy with logistic regression is ", lg.score(x_test, y_test))
 print(metrics.classification_report(y_test, y_pred_lg))
 
 # ========== Decision Trees ==========
 dtc = DecisionTreeClassifier(criterion="entropy", random_state=randomState)
 dtc.fit(x_train, y_train)
 y_pred_dtc = dtc.predict(x_test)
-print("Mean accuracy is ", dtc.score(x_test, y_test))
+print("Mean accuracy with decision trees is ", dtc.score(x_test, y_test))
 print(metrics.classification_report(y_test, y_pred_dtc))
