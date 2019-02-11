3a9cc8070baedb5d3b877787ee6875f34f70d628
Emol <duan.li@mail.mcgill.ca>
2019-02-11 00:25:44 -0500

    load data, XY matrix construction

--- src/data_loading.py
+++ src/data_loading.py
@@ -5,15 +5,15 @@ import json
 
 # @param data_path {string}  the path of the folder that contains the data files.
 # @param sentiment {int} -1: undefined, 0: negative, 1: positive
-# @return data {comment[]} comment.id: file name (e.g. "0", "1"...), comment.txt: comment content (string)
-def load_raw_data(raw_data_path, sentiment = -1, data = []):
+def load_raw_data(raw_data_path, sentiment = -1, data = {'file_names':[], 'target':[], 'data':[]}):
     files = os.listdir(raw_data_path)
     for file in files:
         if file[-4:] == '.txt':
             with open(os.path.join(raw_data_path, file), 'r', encoding='utf8') as f:
-                print("PROCESSING " + raw_dev_data_path + file)
-                comment = {"id": file[:-4], "txt": f.read(), "sentiment": sentiment}
-                data.append(comment)
+                print("PROCESSING " + raw_data_path + file)
+                data['file_names'].append(file[:-4])
+                data['target'].append(sentiment)
+                data['data'].append(f.read())
     return data
 
 
@@ -23,15 +23,17 @@ def write_processed_data(data, outfile_path):
 
 
 def init():
-    train_data = load_raw_data(raw_train_data_path + "/neg/", sentiment=0, data=[])
+    train_data = load_raw_data(raw_train_data_path + "/neg/", sentiment=0, data={'file_names': [], 'target': [], 'data': []})
     write_processed_data(load_raw_data(raw_train_data_path+"/pos/", sentiment=1, data=train_data), train_data_path)
 
-    write_processed_data(load_raw_data(raw_test_data_path, sentiment=-1, data=[]), test_data_path)
-    write_processed_data(load_raw_data(raw_dev_data_path, sentiment=-1, data=[]), dev_data_path)
+    write_processed_data(load_raw_data(raw_test_data_path, sentiment=-1, data={'file_names': [], 'target': [], 'data': []}), test_data_path)
+    write_processed_data(load_raw_data(raw_dev_data_path, sentiment=-1, data={'file_names': [], 'target': [], 'data': []}), dev_data_path)
 
 
 def load_data(path):
     return json.load(open(path, 'r'))
+
+
 
 
 # EXECUTE init() only if data/processed_data/ folder is empty
